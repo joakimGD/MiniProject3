cc9e30917e9c8aadf4fa3cb4312b8b0a0c8eab36
Joakim <jodi@itu.dk>
2021-11-16 11:30:42 +0100

    refactoring proto files

--- server/server.go
+++ server/server.go
@@ -1,9 +1,30 @@
 package main
 
-import(
+import (
+	"context"
+	"log"
+	"math"
 
+	proto "github.com/JoakimGD/MiniProject3/auctionhouse"
 )
 
 type Server struct {
+	proto.UnimplementedAuctionHouseServer
+	local_timestamp int64
+}
+
+func GetTimestamp(s *Server, i int64) int64 {
+	l := float64(s.local_timestamp)
+	i_ := float64(i)
+	f := math.Max(l, i_) + 1
+	return int64(f)
+}
+
+func (s *Server) Bid(ctx context.Context, amount *proto.Amount) (*proto.Ack, error) {
+
+}
+
+func (s *Server) Result(ctx context.Context, amount *proto.Amount) (*proto.Outcome, error) {
 	
 }
+
